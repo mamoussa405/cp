f324e6e4c8c962c7fe8793d05b030874bcd38fcd
Mohamed Amoussaoui <mamoussa@e1r12p2.1337.ma>
2023-05-14 18:36:33 +0100

    added the graph-theory folder in which we will make our graph practice guide.

---
+++ graph-theory/adj-list.cpp
@@ -0,0 +1,325 @@
+// #include <bits/stdc++.h>
+#include "std++.h"
+using namespace std;
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define iter(it, l) for (auto it = l.begin(); it != l.end(); ++it)
+#define riter(it, l) for (auto it = --l.end(); it != l.begin(); --it)
+#define nl "\n"
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define er equal_range
+#define sz(x) (int)(x.size())
+#define L length()
+#define F first
+#define S second
+const int mod  = static_cast<int>(1000000007);
+#define imax INT_MAX
+#define imin INT_MIN
+/*----------------------------- pair<T, T> -------------------------*/
+typedef pair<int, int> pii;
+typedef pair<string, string> pss;
+typedef pair<char, char> pcc;
+typedef pair<int, char> pic;
+typedef pair<char, int> pci;
+/*------------------------------------------------------------------*/
+/*----------------------------- vector<T> -------------------------*/
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pss> vpss;
+typedef vector<pic> vpic;
+typedef vector<pci> vpci;
+typedef vector<vi> vvi;
+typedef vector<vc> vvc;
+typedef vector<vs> vvs;
+/*------------------------------------------------------------------*/
+/*----------------------------- map<T, T> -------------------------*/
+typedef map<int, int> mii;
+typedef map<int, string> mis;
+typedef map<string, int> msi;
+typedef map<char, string> mcs;
+typedef map<string, char> msc;
+/*------------------------------------------------------------------*/
+/*----------------------------- set<T> -------------------------*/
+typedef set<int> si;
+typedef set<string> ss;
+typedef set<char> sc;
+/*------------------------------------------------------------------*/
+/*----------------------------- number types -------------------------*/
+typedef long long ll;
+typedef unsigned long long ull;
+/*------------------------------------------------------------------*/
+ostream& operator<<(ostream& out, vi& v)
+{ 
+    cout << '['; 
+    for(int i = 0; i < sz(v); ++i) 
+    {
+        if (i != sz(v) - 1)
+            cout << v[i] << ", ";
+        else
+            cout << v[i];
+    }
+    cout << "]\n";
+    return out;
+}
+ostream& operator<<(ostream& out, vc& v)
+{ 
+    cout << '['; 
+    for(int i = 0; i < sz(v); ++i) 
+    {
+        if (i != sz(v) - 1)
+            cout << v[i] << ", ";
+        else
+            cout << v[i];
+    }
+    cout << "]\n";
+    return out;
+}
+ostream& operator<<(ostream& out, vs& v)
+{ 
+    cout << '['; 
+    for(int i = 0; i < sz(v); ++i) 
+    {
+        if (i != sz(v) - 1)
+            cout << v[i] << ", ";
+        else
+            cout << v[i];
+    }
+    cout << "]\n";
+    return out;
+}
+ostream& operator<<(ostream& out, vvi& v)
+{ 
+    for(int i = 0; i < sz(v); ++i) 
+    {
+        cout << i << "-->";
+        cout << '[';
+        for(int j = 0; j < sz(v[i]); ++j)
+        {
+            if (j != sz(v[i]) - 1)
+                cout << v[i][j] << " ";
+            else
+                cout << v[i][j];
+        }
+        cout << "]\n";
+    }
+    return out;
+}
+ostream& operator<<(ostream& out, vvc& v)
+{ 
+    for(int i = 0; i < sz(v); ++i) 
+    {
+        cout << i << "-->";
+        cout << '[';
+        for(int j = 0; j < sz(v[i]); ++j)
+        {
+            if (j != sz(v[i]) - 1)
+                cout << v[i][j] << " ";
+            else
+                cout << v[i][j];
+        }
+        cout << "]\n";
+    }
+    return out;
+}
+ostream& operator<<(ostream& out, vvs& v)
+{ 
+    for(int i = 0; i < sz(v); ++i) 
+    {
+        cout << i << "-->";
+        cout << '[';
+        for(int j = 0; j < sz(v[i]); ++j)
+        {
+            if (j != sz(v[i]) - 1)
+                cout << v[i][j] << " ";
+            else
+                cout << v[i][j];
+        }
+        cout << "]\n";
+    }
+    return out;
+}
+ostream& operator<<(ostream& out, vpii& v)
+{ 
+    cout << "[ ";
+    for(int i = 0; i < sz(v); ++i)
+    {
+        if (i != sz(v) - 1)
+            cout << '{' << v[i].F << ", " << v[i].S << '}' << ", ";
+        else
+            cout << '{' << v[i].F << ", " << v[i].S << '}';
+    }
+    cout << " ]\n";
+    return out;
+}
+ostream& operator<<(ostream& out, vpic& v)
+{ 
+    cout << "[ ";
+    for(int i = 0; i < sz(v); ++i)
+    {
+        if (i != sz(v) - 1)
+            cout << '{' << v[i].F << ", " << v[i].S << '}' << ", ";
+        else
+            cout << '{' << v[i].F << ", " << v[i].S << '}';
+    }
+    cout << " ]\n";
+    return out;
+}
+ostream& operator<<(ostream& out, vpci& v)
+{ 
+    cout << "[ ";
+    for(int i = 0; i < sz(v); ++i)
+    {
+        if (i != sz(v) - 1)
+            cout << '{' << v[i].F << ", " << v[i].S << '}' << ", ";
+        else
+            cout << '{' << v[i].F << ", " << v[i].S << '}';
+    }
+    cout << " ]\n";
+    return out;
+}
+ostream& operator<<(ostream& out, vpss& v)
+{ 
+    cout << "[ ";
+    for(int i = 0; i < sz(v); ++i)
+    {
+        if (i != sz(v) - 1)
+            cout << '{' << v[i].F << ", " << v[i].S << '}' << ", ";
+        else
+            cout << '{' << v[i].F << ", " << v[i].S << '}';
+    }
+    cout << " ]\n";
+    return out;
+}
+/*--------------------------- extraction operator overloads -------------------*/
+void operator>>(istream& in, vi& v) { for(auto& x:v) in >> x; }
+void operator>>(istream& in, vc& v) { for(auto& x:v) in >> x; }
+void operator>>(istream& in, vs& v) { for(auto& x:v) in >> x; }
+/*---------------------------------------------------------------------------*/
+ull bexp(ull x, ull n)
+{
+    ull  res = 1;
+    while (n)
+    {
+        if (n&1)
+            res = (res * x) % mod;
+        x = (x * x) % mod;
+        n >>= 1;
+    }
+    return res; 
+}
+
+vi brep(ll n)
+{
+	vi res;
+	for(int i = 63; i >= 0; --i)
+		(n&(1<<i)) ? res.pb(1) : res.pb(0);
+	return res;
+}
+
+void setIO(string name = "")
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    if (name != "")
+    {
+      FILE* cin = freopen(string(name + ".in").c_str(), "r", stdin);
+    //   FILE* cout = freopen(string(name + ".out").c_str(), "w", stdout);
+    }
+}
+
+void bfs(vvc& a, char target)
+{
+    queue<char> q;
+    vector<bool> vis(a.size(), false);
+
+    q.push(target);
+    vis[target - 'A'] = true;
+    while (!q.empty())
+    {
+        char curr = q.front();
+        q.pop();
+        cout << curr << " ";
+        for (int i = 0; i < sz(a[curr - 'A']); ++i)
+        {
+            if (!vis[a[curr - 'A'][i] - 'A'])
+                q.push(a[curr - 'A'][i]),
+                vis[a[curr - 'A'][i] - 'A'] = true;
+        }
+    }
+    cout << endl;
+}
+
+void dfs(vvc& a, char target, vector<bool>& vis)
+{
+    cout << target << " ";
+    for (int i = 0; i < sz(a[target - 'A']); ++i)
+    {
+        if (!vis[a[target - 'A'][i] - 'A'])
+            vis[a[target - 'A'][i] - 'A'] = true,
+            dfs(a, a[target - 'A'][i], vis);
+    }
+}
+
+void dfs_with_stack(vvc& a, char target)
+{
+    stack<char> s;
+    vector<bool> vis(a.size(), false);
+
+    vis[target - 'A'] = true;
+    s.push(target);
+    while (!s.empty())
+    {
+        char curr = s.top();
+        s.pop();
+        
+        cout << curr << " ";
+        for (int i = 0; i < sz(a[curr - 'A']); ++i)
+        {
+            if (!vis[a[curr - 'A'][i] - 'A'])
+                s.push(a[curr - 'A'][i]),
+                vis[a[curr - 'A'][i] - 'A'] = true;
+        }
+    }
+}
+
+void solve(void)
+{
+    int n;
+    int tmp;
+    char target;
+    string s;
+    cin >> n >> target;
+    tmp = n;
+    cin.ignore();
+    vvc a(n);
+    while (n--)
+    {
+        getline(cin, s);
+        for (int i = 1; i < sz(s); ++i)
+        {
+            if (s[i] == ' ' || s[i] == ':')
+                continue;
+            a[s[0] - 'A'].pb(s[i]);
+        }
+    }
+    // vector<bool> vis(tmp, false);
+    // vis[target - 'A'] = true;
+    // dfs(a, target, vis);
+    dfs_with_stack(a, target);
+}
+
+int main(void)
+{
+    setIO("input");
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
